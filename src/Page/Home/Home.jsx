cd5dda41ff0453eda01d319478f1fcb043c8ee9b
arafat248 <aab.arafat248@gmail.com>
2026-05-19 21:41:44 +0600

    Bulid Fetch API

--- src/Page/Home/Home.jsx
+++ src/Page/Home/Home.jsx
@@ -1,10 +1,34 @@
-import Hero from "./HomeComponent/Hero"
+import { useEffect, useState } from "react";
+import Blogs from "./HomeComponent/Blogs";
+import Category from "./HomeComponent/Category";
+import  CategoryBlog  from "./HomeComponent/CategoryBlog";
 
 const Home = () => {
+  const [blogs, setBlogs] = useState([])
+  useEffect(() => {
+    fetch("blogs.json")
+    .then(res => res.json())
+    .then(data => setBlogs(data))
+  },[])
+
+  const [category, setCategoty] = useState([])
+  useEffect(() => {
+    fetch("category.json")
+    .then(res => res.json())
+    .then(data => setCategoty(data))
+  },[])
+
   return (
-    <div>
-      <h1>This is Home page</h1>
-      <Hero />
+    <div className="container mx-auto px-24">
+      <div className="grid grid-cols-16 gap-4 py-10">
+          <div className="col-span-10 border px-4 py-4">
+            <Blogs blogs={blogs}></Blogs>
+          </div>
+          <div className="col-span-6 border px-4 py-4">
+            <Category category={category}></Category>
+            <CategoryBlog></CategoryBlog>
+          </div>
+      </div>
     </div>
   )
 }
