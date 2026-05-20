bd8a68378331583a89549f80d2005c596fb6501e
arafat248 <aab.arafat248@gmail.com>
2026-05-20 22:30:51 +0600

    Add image

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from "./Component/Shared/Header";
 import Footer from "./Component/Shared/Footer";
 import Home from "./Page/Home/Home";
+import TopHeader from "./Component/Shared/TopHeader";
 
 const App = () =>{
     return (
-        <div>
+        <div className="flex-wrap">
+            <TopHeader></TopHeader>
             <Header></Header>
             <Home></Home>
             <Footer></Footer>
